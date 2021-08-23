cb107dec481fb60fd6f9324508af6567c251071b
sahmeder <sahme87@wgu.edu>
2021-08-23 08:20:34 -0400

    dom function controls

--- app.js
+++ app.js
@@ -10,4 +10,9 @@ $(function () {
     var pole_2 = $('#pole_2');
     var restart_btn = $('#restart_btn');
 
-    
+   //variables to access DOM to be used for control functions later
+   var container_width = parseInt(container.width());
+   var container_height = parseInt(container.height());
+   var pole_width = parseInt(pole.width());
+   var bird_height = parseInt(bird.height());
+   var bird_width = parseInt(bird.width());
