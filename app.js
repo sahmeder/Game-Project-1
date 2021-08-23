427cefa8f27fdb915dbbb1da4350bb023a1c84ad
sahmeder <sahme87@wgu.edu>
2021-08-22 21:52:03 -0400

    DOM variables

---
+++ app.js
@@ -0,0 +1,13 @@
+$(function () {
+
+    var animation;
+
+    //assigning DOM objects to variables to make calling DOM easier
+    var container = $('#container');
+    var bird = $('#bird');
+    var pole = $('.pole');
+    var pole_1 = $('#pole_1');
+    var pole_2 = $('#pole_2');
+    var restart_btn = $('#restart_btn');
+
+    
